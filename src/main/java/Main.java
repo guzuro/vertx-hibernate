115aa6d65ffbb654b85fd3ae53843ac2336264d2
guzuro <guzurogmn@gmail.com>
2022-07-21 22:42:11 +0300

    added demo data

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import models.Ticket;
+import models.User;
+import services.UserService;
+
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("qwe");
+        UserService userService = new UserService();
+        User user = new User("Masha", "Egorova");
+        userService.saveUser(user);
+        Ticket ticket = new Ticket("new task", "description", LocalDateTime.now());
+        ticket.setUser(user);
+        user.addTicket(ticket);
+        userService.updateUser(user);
     }
 }
